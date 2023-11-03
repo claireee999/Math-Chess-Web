edfb05cd1bc25445760b8a1b9baf4fbd3dd70e36
claireee999 <105027879+claireee999@users.noreply.github.com>
2023-11-03 00:37:54 -0400

    update

--- src/components/RuleModal/rules.ts
+++ src/components/RuleModal/rules.ts
@@ -1,8 +1,8 @@
 export const rules = {
     strings: [
-        "Move: A piece can move to any of its empty neighbouring positions.",
-        "Jump: A piece can jump over any other single piece.",
-        "Single jump: A piece can jump ",
-        "consecutive jump: "
+        "Move: : Any chess piece can be moved one step to an adjacent empty space.",
+        "Jump: Any chess piece can jump over an adjacent chess piece, but it cannot perform consecutive jumps.",
+        "Single Hop: A single hop refers to a chess piece being able to jump over several other chess pieces in the same straight line. However, the condition is that the numbers of the pieces being jumped over must be obtained through one or more arithmetic operations such as addition, subtraction, multiplication, or division, and the result should be the number of the piece to be hopped over. Single hop is a type of mathematical problem with multiple solutions, meaning that a single problem can have several different solutions. It is often used to develop students' divergent thinking skills. It is also a type of problem where the answer is known, and the goal is to find the algorithm, which helps cultivate students' reverse thinking abilities. ",
+        "Consecutive Hop: Consecutive hop means that a chess piece can continuously hop over segments of chess pieces that can yield the same result. "
     ],
 };
